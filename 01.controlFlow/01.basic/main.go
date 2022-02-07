35d31b70b628c40808cad7a90ad45265c583eeb7
Ramsai <ramsai.lingampalli@zopsmart.com>
2022-02-07 20:54:21 +0530

    renamed file

---
+++ 01.controlFlow/01.basic/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i <= 100; i++ {
+		fmt.Println("Hello, playground")
+	}
+}
+
+/*
+	for init;condition;post{
+		fmt.Println("Hello,playground")
+	}
+*/
