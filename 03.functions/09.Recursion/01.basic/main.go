0d0b589ed9e37bb01762fb9fed0cbda00eb80ae0
Ramsai <ramsai.lingampalli@zopsmart.com>
2022-02-13 22:20:38 +0530

    add recursion

--- 03.functions/09.Recursion/01.basic/main.go
+++ 03.functions/09.Recursion/01.basic/main.go
@@ -1,1 +1,14 @@
-package _1_basic
+package main
+
+import "fmt"
+
+func fact(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * fact(n-1)
+}
+
+func main() {
+	fmt.Println(fact(4))
+}
