856bff8be248694952ffb00e159c50a7c25d763a
Ramsai <ramsai.lingampalli@zopsmart.com>
2022-02-13 22:19:12 +0530

    add closure

--- 03.functions/08.closure/basic/main.go
+++ 03.functions/08.closure/basic/main.go
@@ -1,1 +1,29 @@
-package basic
+package main
+
+import "fmt"
+
+func main() {
+	GFG := 0
+
+	counter := func() int {
+		GFG += 1
+		return GFG
+	}
+
+	fmt.Println(counter())
+	fmt.Println(counter())
+
+	a := incrementor()
+	fmt.Println(a)
+
+	b := a()
+	fmt.Println(b)
+}
+
+func incrementor() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+}
